9ba60292424f8aa7bf4acbe93e52028d6b04eaab
paseek <paseek@yahoo.com>
2022-10-10 11:53:39 +0700

    Исправляет ошибки 2-го ДЗ

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 // Функция возвращает целое число из диапазона положительных чисел где max всегда больше или равен min.
-function randomNumber (min, max) {
-  min = Math.ceil (min);
-  max = Math.floor (max);
-  if (max <= min || max < 0 || min < 0) {
+// function randomNumber (min, max) {
+const getRandomNumber = (min, max) => {
+  const smaller = Math.ceil (min);
+  const bigger = Math.floor (max);
+  if (bigger <= smaller || bigger < 0 || smaller < 0) {
     return NaN;
   }
-  return Math.floor((Math.random() * (max - min + 1)) + min);
-}
+  return Math.floor((Math.random() * (bigger - smaller + 1)) + smaller);
+};
 
 // Функция возвращает число с указанным количеством знаков после запятой из диапазона положительных чисел где max всегда больше или равен min.
-function randomGeoNumber (min, max, digits) {
-  min = Math.ceil (min);
-  max = Math.floor (max);
-  if (max <= min || max < 0 || min < 0) {
+const getRandomGeo = (min, max, digits) => {
+  const smaller = min;
+  const bigger = max;
+  if (bigger <= smaller || bigger < 0 || smaller < 0) {
     return NaN;
   }
-  return (Math.random() * (max - min + 1) + min).toFixed(digits);
-}
+  return (Math.random() * (bigger - smaller + 1) + smaller).toFixed(digits);
+};
 
-randomNumber(0, 1);
-randomGeoNumber(0, 100, 10);
+getRandomNumber(0, 1);
+getRandomGeo(0, 100, 10);
