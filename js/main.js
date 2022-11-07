1387d731033f56e95a287db788bdeea52351e483
paseek <paseek@yahoo.com>
2022-11-07 12:46:53 +0300

    Добавляет синхронизацию checkin/out

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ import { getOffers } from './data.js';
 import { makeFormInactive, makeFormActive, makeMapInactive, makeMapActive } from './form.js';
 import { renderSimilarOffers } from './popup.js';
 
+
 makeFormInactive();
 makeMapInactive();
+
 
 renderSimilarOffers(getOffers());
 
 makeFormActive();
 makeMapActive();
-
